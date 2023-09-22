732f839a6de5c57a004569701dbb8815275cfb20
Justin Elvin <75230614+JustinElvin@users.noreply.github.com>
2023-09-22 16:04:46 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+//ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//klik diluar sidebar untuk menghilangkan nav
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
